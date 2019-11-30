d1aa706ad028268e466183e119f40c3f90dae2cd
alexey-ringo <alexey.ringo@gmail.com>
2019-11-30 22:19:10 +0000

    http PSR7 interface

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+use Framework\Http\RequestFactory;
+use Framework\Http\Response;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+### Initialization
+$request = RequestFactory::fromGlobals();
+
+### Action
+$name = $request->getQueryParams()['name'] ?? 'Guest';
+$response = (new Response('Hello, ' . $name . '!'))
+    ->withHeader('X-Developer', 'AlexRingo');
+    
+### Sending
+header('HTTP/1.0 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+foreach ($response->getHeaders() as $name => $values) {
+    header($name . ':' . implode(', ', $values));
+}
+echo $response->getBody();
